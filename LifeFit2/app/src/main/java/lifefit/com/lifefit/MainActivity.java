0cf254c969f0620b826ba97d4436e6eea4882d31
Manuel Samudio <manuxscorpiuz12@gmail.com>
2015-09-20 20:54:28 -0500

    Drawer Menu
    menu que se desliza desde el borde izquierdo.
    Custom Nav Bar

--- LifeFit2/app/src/main/java/lifefit/com/lifefit/MainActivity.java
+++ LifeFit2/app/src/main/java/lifefit/com/lifefit/MainActivity.java
@@ -1,16 +1,30 @@
 package lifefit.com.lifefit;
 
+
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+    private  String[] menu_drawer;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        menu_drawer = getResources().getStringArray(R.array.menu_drawer);
+        listView = (ListView)findViewById(R.id.left_drawer_list);
+        listView.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, menu_drawer));
+        listView.setOnItemClickListener(this);
     }
 
     @Override
@@ -25,13 +39,32 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        displayView(position);
+    }
+
+    private void displayView(int position) {
+        switch (position) {
+            case 0:
+                intent = new Intent(this, Caminar.class);
+                startActivity(intent);
+                finish();
+                break;
+            case 1:
+                intent = new Intent(this, Correr.class);
+                startActivity(intent);
+                finish();
+                break;
+            case 2:
+                intent = new Intent(this, Bicicleta.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+
 }
